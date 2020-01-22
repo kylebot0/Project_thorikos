ef12f098eef94e5209d2445af94d8446ff10d5f0
kylebot0 <kylebot07@gmail.com>
2020-01-22 14:32:14 +0100

    Fix bars changing

--- src/script/index.js
+++ src/script/index.js
@@ -13,7 +13,10 @@ map.scrollZoom.disable();
 map.addControl(new mapboxgl.NavigationControl());
 
 const container = map.getCanvasContainer();
-const selectSVG = d3.select(map.getCanvasContainer()).append("svg").attr("class", "map");
+const selectSVG = d3
+  .select(map.getCanvasContainer())
+  .append("svg")
+  .attr("class", "map");
 const tooltip = d3
   .select("body")
   .append("div")
@@ -58,7 +61,6 @@ function nestData(rawData) {
 
 function makeFilters(gridData, rawData) {
   let cleanData = nestData(rawData);
-
 
   let SHAPE_OBJECT = d3
     .nest()
@@ -139,16 +141,19 @@ function makeFilters(gridData, rawData) {
   });
   openMenu();
   lowerCase(rawData);
+
+  //New rawdata for filters to add
   let newRawData = [...rawData];
-  // console.log(newRawData)
+
   filter(gridData, filters, rawData, newRawData);
-  filterCategory.forEach(function(category, i){
-    makeBars(filters, category, i)
-  })
+  filterCategory.forEach(function(category, i) {
+    makeBars(filters, category, i);
+  });
+  updateBars(filters);
 }
 
 function makeBars(filters, category, i) {
-  console.log(filters[i])
+  console.log(filters[i]);
   let barChart = d3
     .select(`.filter-chart-container-${category}`)
     .append("svg")
@@ -156,59 +161,79 @@ function makeBars(filters, category, i) {
     .attr("width", "100%")
     .attr("height", "100%")
     .append("g");
-  // let margin = 200;
   let width = 50;
-  d3.selectAll(".filter-container").attr("class", "filter-container show")
-  d3.selectAll(`.filter-container-${category}`).attr("class", `filter-container-${category} show`);
+
+  d3.selectAll(".filter-container").attr("class", "filter-container show");
+  d3.selectAll(`.filter-container-${category}`).attr(
+    "class",
+    `filter-container-${category} show`
+  );
+
   let height = d3
     .select(`.filter-chart-container-${category}`)
     .node()
     .getBoundingClientRect().height;
-  // console.log(maxHeight)
-  d3.selectAll(".filter-container").attr("class", "filter-container")
-  d3.selectAll(`.filter-container-${category}`).attr("class", `filter-container-${category}`);
-  var maximumY = d3.max(filters[0], function (d) {
+
+  d3.selectAll(".filter-container").attr("class", "filter-container");
+  d3.selectAll(`.filter-container-${category}`).attr(
+    "class",
+    `filter-container-${category}`
+  );
+
+  var maximumY = d3.max(filters[0], function(d) {
     return d.values.length;
   });
 
-  var y = d3.scaleBand()
+  var y = d3
+    .scaleBand()
     .range([0, height])
     .padding(0);
 
-  var x = d3.scaleLinear()
-    .range([0, width]);
+  var x = d3.scaleLinear().range([0, width]);
 
   x.domain([
     -(maximumY * 0.03),
-    (d3.max(filters[i], function(d) {
+    d3.max(filters[i], function(d) {
       return d.values.length;
+    }) / 2
+  ]);
+  y.domain(
+    filters[i].map(function(d) {
+      return d.key;
     })
-  ) / 2]);
-  y.domain(filters[i].map(function (d) { return d.key; }));
+  );
 
-  let bars = barChart.selectAll(".bar")
+  let bars = barChart
+    .selectAll(".bar")
     .data(filters[i])
     .enter()
-    .append("g")
+    .append("g");
 
   bars
     .append("rect")
     .attr("class", "bar")
-    .attr("width", function (d) { return x(d.values.length); })
-    .attr("y", function (d) { return y(d.key); })
+    .attr("id", function(d) {
+      return d.key;
+    })
+    .attr("width", function(d) {
+      return x(d.values.length);
+    })
+    .attr("y", function(d) {
+      return y(d.key);
+    })
     .attr("height", "12")
-    .style("fill", "lightgrey")
+    .style("fill", "lightgrey");
 
-
-  bars.append("text")
+  bars
+    .append("text")
     .attr("class", "label")
-    .attr("y", function (d) {
+    .attr("y", function(d) {
       return y(d.key) + 10;
     })
-    .attr("x", function (d) {
-      return 100;
+    .attr("x", function(d) {
+      return 110;
     })
-    .text(function (d) {
+    .text(function(d) {
       return d.values.length;
     });
 }
@@ -221,15 +246,20 @@ function appendFilters(filter, category) {
     .append("label")
     .attr("class", "filter-option")
     .html(function(d) {
+      let key = d.key;
+      key = key.replace(/[/]/g, "_");
+      key = key.replace(/[ ]/g, "-");
+      key = key.replace(/[+]/g, "p");
+      key = key.replace(/[?]/g, "q");
       return (
-        `<input id="${d.key}" class="filter" type="checkbox" checked="checked"> ` +
+        `<input id="${key}" class="filter" type="checkbox" checked="checked"> ` +
         d.key
       );
     });
 }
 
 function openMenu() {
-  d3.selectAll("button").on("click", function() {
+  d3.selectAll(".menu").on("click", function() {
     let element = this.nextElementSibling;
     let childElement = this.nextElementSibling.children[0];
     if (element.classList.contains("show")) {
@@ -241,13 +271,11 @@ function openMenu() {
       element.classList.toggle("show");
       childElement.classList.toggle("show");
     }
-    
   });
-
 }
 
 function filter(gridData, filters, data, cleanData) {
-  d3.selectAll(".fast-filter").on("click", function () {
+  d3.selectAll(".fast-filter").on("click", function() {
     if (this.id == "fast-filter-all") {
       data = cleanData;
       d3.select("#fast-filter-none").property("checked", false);
@@ -257,7 +285,7 @@ function filter(gridData, filters, data, cleanData) {
       filteredArray = nestedArray;
       makeGrid(gridData, filteredArray);
     } else if (this.id == "fast-filter-none") {
-      data = []
+      data = [];
       d3.select("#fast-filter-all").property("checked", false);
       d3.select(this).property("checked", true);
       d3.selectAll(".filter").property("checked", false);
@@ -266,11 +294,14 @@ function filter(gridData, filters, data, cleanData) {
     }
   });
 
-
   filters = filters.flat();
   d3.selectAll(".filter").on("click", function() {
     let filterButton = this;
     let category = this.id;
+    category = category.replace(/[_]/g, "/");
+    category = category.replace(/[ ]/g, "-");
+    category = category.replace(/[+]/g, "p");
+    category = category.replace(/[?]/g, "q");
     let filteredArray = [];
     if (this.checked) {
       cleanData.forEach(function(item, i) {
@@ -292,8 +323,11 @@ function filter(gridData, filters, data, cleanData) {
       });
       let nestedArray = nestData(data);
       filteredArray = nestedArray;
+
+      updateBars(filters, category, filterButton, data);
       makeGrid(gridData, filteredArray);
     } else if (!this.checked) {
+      // console.log(this)
       data = data.filter(function(item) {
         if (category === item.CHRONOLOGY) {
           return false;
@@ -311,14 +345,47 @@ function filter(gridData, filters, data, cleanData) {
           return true;
         }
       });
-      console.log(data);
+      // console.log(data);
       let nestedArray = nestData(data);
       filteredArray = nestedArray;
     } else {
       return;
     }
+    updateBars(filters, category, filterButton), data;
     makeGrid(gridData, filteredArray);
   });
+}
+
+function updateBars(filters, category, filterButton, data) {
+  // console.log(category)
+  d3.selectAll(".bar")
+    .transition()
+    .duration(500)
+    .style("fill", function() {
+      // console.log(filters)
+      let bar = this;
+      let fill = ""
+      filters = filters.flat()
+      filters.forEach(function(item, i) {
+          let category = item.key
+              category = category.replace(/[/]/g, "_");
+              category = category.replace(/[ ]/g, "-");
+              category = category.replace(/[+]/g, "p");
+              category = category.replace(/[?]/g, "q");
+          if (d3.select(bar).property("id") == item.key) {
+            // console.log(category);
+            let inputChecked = d3.select(`#${category}`);
+            console.log(inputChecked)
+            if (inputChecked.property("checked") == false) {
+              fill = "lightgrey";
+            } else if (inputChecked.property("checked") == true) {
+              fill = "#D95236";
+            }
+          } else {
+          }
+        });
+        return fill;
+    });
 }
 
 function lowerCase(data) {
@@ -330,19 +397,20 @@ function lowerCase(data) {
 function makeGrid(gridData, data) {
   render(true);
   function render(render) {
-    console.log(data)
     const totalItems = totalFound(data);
     const maxValue = d3.max(data, function(d) {
       return d.values.length;
     });
-    const color = d3
-      .scaleLinear()
-      .range(["#fff", "#fd8d3c"])
-      .domain([0, maxValue]);
+    const pivotArray = makePivotArray(maxValue);
 
     d3Projection = getD3();
     const path = d3.geoPath();
+
     if (render == true) {
+      //Deleting everything on re-render
+      d3.select(".legend")
+        .remove()
+        .exit();
       selectSVG
         .selectAll("path")
         .remove()
@@ -357,6 +425,12 @@ function makeGrid(gridData, data) {
         .exit();
       selectSVG
         .selectAll("text")
+        .remove()
+        .exit();
+    }
+    if (data.length == 0) {
+      console.log("test");
+      d3.selectAll(".legend")
         .remove()
         .exit();
     }
@@ -379,27 +453,14 @@ function makeGrid(gridData, data) {
         tooltip
           .transition()
           .duration(200)
-          .style("opacity", 0.9);
+          .style("opacity", 1);
 
         tooltip
           .html(function() {
-            let contextNr =
-              "T12-" + d.properties.context + "-" + d.properties.mesoindex;
-            if (d.properties.context == undefined) {
-              return (
-                "<p>" +
-                d.properties.name +
-                d.properties.row +
-                "-" +
-                d.properties.mesoindex +
-                "</p>"
-              );
-            } else {
-              return "<p>" + contextNr + "</p>";
-            }
+            return makeTooltip(d, data);
           })
-          .style("left", d3.event.pageX - 60 + "px")
-          .style("top", d3.event.pageY - 64 + "px");
+          .style("left", d3.event.pageX - 97 + "px")
+          .style("top", d3.event.pageY - 270 + "px");
       })
       .on("mouseout", d => {
         tooltip
@@ -408,29 +469,16 @@ function makeGrid(gridData, data) {
           .style("opacity", 0);
       })
       .style("fill", d => {
-        let context =
-          "T12-" + d.properties.context + "-" + d.properties.mesoindex;
-        if (d.properties.context === undefined) {
-          return "white";
-        } else if (data.length == 0) {
-          return "white";
+        return matchContextToPath(data, d, pivotArray, maxValue);
+      })
+      .style("fill-opacity", function() {
+        let fill = d3.select(this).style("fill");
+        if (fill == "white") {
+          return 0.6;
         } else {
-          let fill = "";
-          let boolean = false;
-          data.forEach(function(item, i) {
-            if (context === item.key) {
-              boolean = true;
-              fill = color(item.values.length);
-            } else if (boolean == false) {
-              fill = "white";
-            } else {
-              return;
-            }
-          });
-          return fill;
+          return 1;
         }
       })
-      .style("fill-opacity", "0.8")
       .style("stroke", "#fff")
       .style("stroke-width", "1")
       .style("stroke-opacity", "0.3");
@@ -441,6 +489,9 @@ function makeGrid(gridData, data) {
       .text(function() {
         return totalItems + " finds";
       });
+
+    //Make legend
+    makeLegend(data, pivotArray, maxValue);
   }
 
   map.on("viewreset", function() {
@@ -449,6 +500,184 @@ function makeGrid(gridData, data) {
   map.on("move", function() {
     render(true);
   });
+}
+
+function matchContextToPath(data, d, pivotArray, maxValue) {
+  const color = d3
+    .scaleLinear()
+    .range([
+      "#fff",
+      "#FCF9D0",
+      "#FBE2A3",
+      "#F7C687",
+      "#F2AB6D",
+      "#EB9052",
+      "#E3723C",
+      "#D95236"
+    ])
+    .domain([
+      0,
+      pivotArray[6],
+      pivotArray[5],
+      pivotArray[4],
+      pivotArray[3],
+      pivotArray[2],
+      pivotArray[1],
+      pivotArray[0],
+      maxValue
+    ]);
+
+  let context = "T12-" + d.properties.context + "-" + d.properties.mesoindex;
+  if (d.properties.context === undefined) {
+    return "white";
+  } else if (data.length == 0) {
+    return "white";
+  } else {
+    let fill = "";
+    let boolean = false;
+    data.forEach(function(item, i) {
+      if (context === item.key) {
+        boolean = true;
+        fill = color(item.values.length);
+      } else if (boolean == false) {
+        fill = "white";
+      } else {
+        return;
+      }
+    });
+    return fill;
+  }
+}
+
+function makeTooltip(d, data) {
+  console.log(d);
+  if (d.properties.context == undefined) {
+    return (
+      "</div class='tooltip-container'><h2>" +
+      d.properties.name +
+      d.properties.row +
+      "-" +
+      d.properties.mesoindex +
+      "</h2><p>" +
+      "0" +
+      "</p></div>"
+    );
+  } else {
+    let contextNr =
+      "T12-" + d.properties.context + "-" + d.properties.mesoindex;
+    let objectsFound = 0;
+
+    data.forEach(function(item, i) {
+      if (contextNr === item.key) {
+        boolean = true;
+        objectsFound = item.values.length;
+      } else {
+        return;
+      }
+    });
+
+    return (
+      "<h2>" +
+      contextNr +
+      "</h2><div class='tooltip-container'><h3>" +
+      objectsFound +
+      "</h3>" +
+      "<p>FOUND OBJECTS*</p>" +
+      "</div><div class='tooltip-container'>" +
+      +"</div>"
+    );
+  }
+}
+
+function makePivotArray(maxValue) {
+  let pivotArray = [maxValue];
+  let pivotNumber = maxValue;
+
+  for (let i = 0; i < 7; i++) {
+    pivotNumber = Math.round(pivot(pivotNumber));
+    pivotArray.push(pivotNumber);
+  }
+  return pivotArray;
+}
+
+function makeLegend(data, pivotArray, maxValue) {
+  const color = d3
+    .scaleLinear()
+    .range([
+      "#fff",
+      "#FCF9D0",
+      "#FBE2A3",
+      "#F7C687",
+      "#F2AB6D",
+      "#EB9052",
+      "#E3723C",
+      "#D95236"
+    ])
+    .domain([
+      0,
+      pivotArray[6],
+      pivotArray[5],
+      pivotArray[4],
+      pivotArray[3],
+      pivotArray[2],
+      pivotArray[1],
+      pivotArray[0],
+      maxValue
+    ]);
+  pivotArray = pivotArray.reverse(pivotArray.push(0));
+  let width = 30;
+  let height = 20;
+  let x = 0;
+
+  let legend = d3
+    .select(".legend-container")
+    .append("svg")
+    .attr("class", "legend")
+    .attr("height", "12vh")
+    .attr("width", "300");
+
+  legend
+    .selectAll("rect")
+    .data(pivotArray)
+    .enter()
+    .append("rect")
+    .attr("height", height)
+    .attr("width", width)
+    .attr("x", function() {
+      x = x + width;
+      return x;
+    })
+    .attr("y", "30")
+    .style("fill", function(d) {
+      return color(d);
+    });
+
+  x = 10;
+
+  legend
+    .selectAll("text")
+    .data(pivotArray)
+    .enter()
+    .append("text")
+    .attr("class", "legend-label")
+    .attr("x", function() {
+      x = x + width;
+      return x;
+    })
+    .attr("y", "65")
+    .text(function(d) {
+      console.log(d);
+      if (isNaN(d)) {
+        return "";
+      } else {
+        return d;
+      }
+    });
+}
+
+function pivot(val) {
+  val = val * 0.75;
+  return val;
 }
 
 function totalFound(data) {
